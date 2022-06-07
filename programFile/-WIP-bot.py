4dfd9928d84afc4cddc24899fc878e14671e64af
disbotMust <b08170076@std.must.edu.tw>
2022-06-07 13:49:55 +0800

    Update -WIP-bot.py

--- programFile/-WIP-bot.py
+++ programFile/-WIP-bot.py
@@ -20,4 +20,4 @@ async def on_message(message):
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
 
-client.run('your token here')
+client.run('OTgzNjA0Nzc4MjAwMzY3MTA0.GBBTjV.R6-so8-gNEVlZU_E76KKw6crKQ20KiZ-psxUUE')
