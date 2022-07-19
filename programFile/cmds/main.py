2293624437589ae2ff71614b1d11268076af11f6
disbotMust <104551519+disbotMust@users.noreply.github.com>
2022-07-19 09:49:22 +0800

    Add: some code text
    增加註解

--- programFile/cmds/main.py
+++ programFile/cmds/main.py
@@ -1,12 +1,18 @@
 import discord
+import json
 from discord.ext import commands
 from core.classes import Cog_Extension
 
+#打開json並讀取，使用utf8 encode
+with open('setting.json',mode='r',encoding='utf8') as jFile:
+    jdata = json.load(jFile)
+
 class Main(Cog_Extension):
     @commands.command()
+#顯示延遲
     async def ping(self,ctx):
         await ctx.send(f'{round(self.bot.latency*1000)} (ms)')
-    
+#測試用指令
     @commands.command()
     async def hi(self, ctx):
         await ctx.send("ABCC")
@@ -22,6 +28,14 @@ class Main(Cog_Extension):
     @commands.command()
     async def clean (self, ctx, num:int):
         await ctx.channel.purge(limit=num+1)
+"""#更改json 監聽頻道ID
+    @commands.command()
+    async def listID(self ,ctx, msg, msg2):
+        for test in jdata:
+            if test == str(msg):
+                jdata[test] = int(msg2)
+        await ctx.send("listen_channelID更改完成")"""
+
 
 
 def setup(bot):
