0875188bf504f22a930bb00c260b355770d2b845
disbotMust <104551519+disbotMust@users.noreply.github.com>
2022-07-25 09:26:12 +0800

    Create WIP_other_porgram.py
    建立新模塊(module)方便測試程式溝通

---
+++ programFile/WIP_other_porgram.py
@@ -0,0 +1,11 @@
+import datetime
+
+
+class test:
+    def __init__(self, search):
+        self.search = search
+    def getTime():
+        print (str(datetime.datetime.now()))
+    def google(self):
+        google = "https://www.google.com/search?q="+str(self.search)
+        return google
