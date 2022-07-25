50482e386cb61b94d069c4904999e70c396518a1
disbotMust <104551519+disbotMust@users.noreply.github.com>
2022-07-25 11:30:52 +0800

    Update react.py
    轉移main的測試指令

--- programFile/cmds/react.py
+++ programFile/cmds/react.py
@@ -6,6 +6,10 @@ class React(Cog_Extension):
     @commands.command()
     async def test(self, ctx, arg):
         await ctx.send(arg)
+#測試用指令
+    @commands.command()
+    async def hi(self, ctx):
+        await ctx.send("ABCC")
 
 def setup(bot):
     bot.add_cog(React(bot))
