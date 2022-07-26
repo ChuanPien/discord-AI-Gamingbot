82c3a2c61107639781901155869b33c2972d1f2f
disbotMust <104551519+disbotMust@users.noreply.github.com>
2022-07-26 11:38:02 +0800

    remove : getTime func
    移除getTime函數，轉移至classes中

--- programFile/WIP_other_porgram.py
+++ programFile/WIP_other_porgram.py
@@ -1,11 +1,8 @@
-import datetime
 
 
 class test:
     def __init__(self, search):
         self.search = search
-    def getTime():
-        print (str(datetime.datetime.now()))
     def google(self):
         #.replace(" ","+") 將空白字元轉換成google網址的空白搜尋替代字元
         #以便正確顯示搜尋的相關網址
