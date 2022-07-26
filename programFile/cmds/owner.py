ecf8ad2ebc08a7ccf73c05272fd724809e25cc58
disbotMust <104551519+disbotMust@users.noreply.github.com>
2022-07-26 11:34:23 +0800

    test cmd
    新增此項，重要功能只有owner能使用，方便測試

---
+++ programFile/cmds/owner.py
@@ -0,0 +1,59 @@
+import discord
+import json,asyncio, os
+from discord.ext import commands
+from matplotlib.pyplot import close
+from core.classes import Cog_Extension,Global_Func
+
+# @commands.is_owner() >> 辨別是否為擁有者
+#打開json並讀取，使用utf8 encode
+with open('setting.json',mode='r',encoding='utf8') as jFile:
+    jdata = json.load(jFile)
+
+class Owner(Cog_Extension):
+#load模塊(Cog)
+    @commands.command()
+    @commands.is_owner()
+    async def load(self, ctx, extension):
+        self.bot.load_extension(f"cmds.{extension}")
+        await ctx.send(f"Loaded {extension} done.")
+#reload模塊(Cog)
+    @commands.command()
+    @commands.is_owner()
+    async def reload(self, ctx, extension):
+        if extension == "*":
+#load cmds資料夾內所有cog
+            for filename in os.listdir("./cmds"):
+                if filename.endswith('.py'):
+                    self.bot.load_extension(f"cmds.{filename[:-3]}")
+            await ctx.send(f"RE-Loaded ALL done.")
+        else:
+            self.bot.reload_extension(f"cmds.{extension}")
+            await ctx.send(f"RE-Loaded {extension} done.")
+#unload模塊(Cog)
+    @commands.command()
+    @commands.is_owner()
+    async def unload(self, ctx, extension):
+        self.bot.unload_extension(f"cmds.{extension}")
+        await ctx.send(f"un-Loaded {extension} done.")
+    
+    @commands.command()
+    @commands.is_owner()
+    async def shutdown(self, ctx):
+        await ctx.send("Shutdown now..... Pls wait")
+        await asyncio.sleep(1)
+        await self.bot.close()
+
+#取得使用此指令的頻道ID
+    @commands.command()
+    @commands.is_owner()
+    async def gChId(self,ctx):
+        await ctx.send(f'This Chennel ID is : {ctx.channel.id}')
+#取得使用指令的使用者ID
+    @commands.command()
+    @commands.is_owner()
+    async def yourID(self,ctx):
+        await ctx.send(f'Your ID is : {ctx.author.id}')
+
+
+def setup(bot):
+    bot.add_cog(Owner(bot))
