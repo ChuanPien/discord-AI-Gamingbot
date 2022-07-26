e0706736f9f30bb0c9665e69c10aff7db5b0419a
disbotMust <104551519+disbotMust@users.noreply.github.com>
2022-07-26 11:34:57 +0800

    Updata : new class
    新增class 自定功能使用

--- programFile/core/classes.py
+++ programFile/core/classes.py
@@ -1,6 +1,16 @@
 import discord
 from discord.ext import commands
+import datetime
 
 class Cog_Extension(commands.Cog):
+    #用於Cog繼承
     def __init__(self,bot):
         self.bot = bot
+
+class Global_Func():
+    #自定義常用功能
+
+    #取得現在時間
+    def getTime():
+        return str(datetime.datetime.now())
+    
