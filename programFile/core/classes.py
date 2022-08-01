e5bc1416dfc0a7af6157039c1360bc3a731575cc
disbotMust <104551519+disbotMust@users.noreply.github.com>
2022-08-01 11:58:17 +0800

    Add logMessage fun
    新增message選擇器

--- programFile/core/classes.py
+++ programFile/core/classes.py
@@ -13,4 +13,20 @@ class Global_Func():
     #取得現在時間
     def getTime():
         return str(datetime.datetime.now())
-    
+    #msg訊息發送選擇
+    def logMessage(msg,authorNick,authorName,guildName,chatName,chatId,guildMembers):
+        msgJson = [authorNick,authorName,guildName,chatName,chatId,guildMembers]
+        msgSendNick = "\n訊息發送者(伺服器暱稱) : " + str(msg.author.nick)
+        msgSendName = "\n訊息發送者 : " + str(msg.author.name)
+        msgContent = "\n訊息 : " + str(msg.content)
+        authorGuildName = "\n訊息伺服器 : "+ str(msg.author.guild.name)
+        channelName = "\n訊息頻道 : " + str(msg.channel.name)
+        channelId = "\n訊息頻道ID : "+ str(msg.channel.id)
+        memberCount = "\n成員數 : "+ str(msg.author.guild.member_count)
+        printList = [msgSendNick,msgSendName,msgContent,authorGuildName,channelName,channelId,memberCount]
+        returnText = ""
+        for lists in range(len(msgJson)):
+            if msgJson[lists] != 1:
+                continue
+            returnText = returnText+str(printList[lists])
+        return returnText
