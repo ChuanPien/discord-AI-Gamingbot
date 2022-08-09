a4b5030ba864078310d027db446c50f29e6911ee
disbotMust <104551519+disbotMust@users.noreply.github.com>
2022-08-09 13:23:25 +0800

    testFiles

--- programFile/WEB_REQUESTS/API研究.md
+++ programFile/WEB_REQUESTS/API研究.md
@@ -22,5 +22,8 @@ get infomasion: https://web.poe.garena.tw/api/trade/fetch/
 將上述網址 /fetch後段加上 result(10) + queryID，即可取得查詢資料
 >無需使用 payload
 ## 整理資料(待補)
->暫定
+>小教學https://www.reddit.com/r/pathofexiledev/comments/7aiil7/how_to_make_your_own_queries_against_the_official/
+
+- https://web.poe.garena.tw/api/trade/data/stats
+  - 屬性json(官方API)
 丟查詢結果網址
--- programFile/WEB_REQUESTS/requests_post.py
+++ programFile/WEB_REQUESTS/requests_post.py
@@ -1,11 +1,41 @@
 import requests
+from time import sleep
+import json
 
 if __name__== "__main__":
+    with open('query.json',mode='r',encoding='utf8') as jFile:
+        jdata = json.load(jFile)
+    sleep(5)
     headers = {
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.134 Safari/537.36 Edg/103.0.1264.77'
+        Accept: */*,
+        Accept-Encoding: gzip, deflate, br,
+        Accept-Language: zh-TW,zh;q=0.9,
+        Connection: keep-alive,
+        Content-Length: 118,
+        Content-Type: application/json,
+        Cookie: POESESSID=ccbd59dbab4ed0c727692ed1bec70926,
+        Host: web.poe.garena.tw,
+        Origin: https://web.poe.garena.tw,
+        Referer: https://web.poe.garena.tw/trade/search/%E5%AE%88%E6%9C%9B%E8%81%AF%E7%9B%9F,
+        sec-ch-ua: ".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103",
+        sec-ch-ua-mobile: ?0,
+        sec-ch-ua-platform: "Windows",
+        Sec-Fetch-Dest: empty,
+        Sec-Fetch-Mode: 'cors',
+        Sec-Fetch-Site: 'same-origin',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
+        'X-Requested-With': 'XMLHttpRequest'
     }
-    post_url = "https://web.poe.garena.tw/api/trade/search/%E5%AE%88%E6%9C%9B%E8%81%AF%E7%9B%9F"
-    data = {
-        
-    }
-
+    post_url = "https://web.poe.garena.tw/api/trade/search/守望聯盟"
+    test_url = "https://www.pathofexile.com/api/trade/search/Standard"
+    data = jdata
+    reponse = requests.post(url=test_url,data=data,headers=headers)
+    reasonPost = reponse.reason
+    start = reponse.status_code
+    
+    #reponse.json() 取得交易頁面數據用
+    #reponse = reponse.json()
+    print(str(reasonPost))
+    print("code:" +str(start))
+    #print("reponse : "+str(reponse))
+    print(reponse.text)
