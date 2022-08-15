a57d285d0e32abfb30d153f42ffb6b68002aa161
disbotMust <104551519+disbotMust@users.noreply.github.com>
2022-08-15 13:15:33 +0800

    Update requests_post.py

--- programFile/WEB_REQUESTS/requests_post.py
+++ programFile/WEB_REQUESTS/requests_post.py
@@ -5,31 +5,21 @@ import json
 if __name__== "__main__":
     with open('query.json',mode='r',encoding='utf8') as jFile:
         jdata = json.load(jFile)
-    sleep(5)
+    #sleep(5)
     headers = {
-        Accept: */*,
-        Accept-Encoding: gzip, deflate, br,
-        Accept-Language: zh-TW,zh;q=0.9,
-        Connection: keep-alive,
-        Content-Length: 118,
-        Content-Type: application/json,
-        Cookie: POESESSID=ccbd59dbab4ed0c727692ed1bec70926,
-        Host: web.poe.garena.tw,
-        Origin: https://web.poe.garena.tw,
-        Referer: https://web.poe.garena.tw/trade/search/%E5%AE%88%E6%9C%9B%E8%81%AF%E7%9B%9F,
-        sec-ch-ua: ".Not/A)Brand";v="99", "Google Chrome";v="103", "Chromium";v="103",
-        sec-ch-ua-mobile: ?0,
-        sec-ch-ua-platform: "Windows",
-        Sec-Fetch-Dest: empty,
-        Sec-Fetch-Mode: 'cors',
-        Sec-Fetch-Site: 'same-origin',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36'
-        'X-Requested-With': 'XMLHttpRequest'
+        'Accept': '*/*',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'zh-TW,zh;q=0.9',
+        'Connection': 'keep-alive',
+        'Content-Length': '118',
+        'Content-Type': 'application/json',
+        'Cookie': '_ga=GA1.2.1222773361.1658121743; stored_data=1; POESESSID=d06178e937205232066827d96b54ba76; _gid=GA1.2.1237697955.1660530539; _gat=1',
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.81 Safari/537.36 Edg/104.0.1293.47'
     }
     post_url = "https://web.poe.garena.tw/api/trade/search/守望聯盟"
     test_url = "https://www.pathofexile.com/api/trade/search/Standard"
     data = jdata
-    reponse = requests.post(url=test_url,data=data,headers=headers)
+    reponse=requests.post(url=post_url,data=data,headers=headers)
     reasonPost = reponse.reason
     start = reponse.status_code
     
