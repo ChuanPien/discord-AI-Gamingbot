eb137109767e67e6e708472f125ecad78f526d8e
disbotMust <104551519+disbotMust@users.noreply.github.com>
2022-08-22 14:12:47 +0800

    updata msg log test

--- programFile/cmds/event.py
+++ programFile/cmds/event.py
@@ -3,7 +3,7 @@ import json
 import datetime # 引入datetime
 from discord.ext import commands
 from cmds.owner import Owner
-from core.classes import Cog_Extension,Global_Func
+from core.classes import Cog_Extension,Global_Func,messageFormat
 from time import sleep
 
 
@@ -17,15 +17,32 @@ class Event(Cog_Extension):
     #on_massage功能必須寫在同一處 不然會覆蓋
     async def on_message(self, msg):
         #設定傳送消息頻道
-        sendContentChannel = self.bot.get_channel(jdata['Trans_channelID'])
+        sendContentChannel = self.bot.get_channel(jdata['listen_test_channelID'])
+        #初始化，設定專用log頻道
+        logChat = self.bot.get_channel(jdata['Log_chat'])
+        #msg抓取初始化
+        if msg.author != self.bot.user:
+            msgFormat = msg.content
+            msgFormat = msgFormat.split(" ")
+            print(msgFormat)
         #測試用指令
         if msg.content == 'Test' or msg.content == 'test' and msg.author != self.bot.user and msg.author.name == "must505":
             await msg.channel.send('Hello!')
-        """if msg.content == '查詢' and msg.author != self.bot.user and msg.author.name == "must505":
-            await msg.channel.send('Test Done!')"""
+        #測試log運作
+        if msg.author != self.bot.user:
+            for mf in msgFormat:
+                if mf == 'sec' :
+                #debug
+                    await msg.channel.send("log運作測試")
+                    #初始化messageFormat
+                    msg_f = messageFormat(msg)
+                    msg_f.log()
+                    await logChat.send("－－－－－\n"+"ID: "+msg_f.getUserId()+"\nMsg: "+msg_f.getMsg()+"\n－－－－－")
+                else:
+                    print("\n\nError")
             
         #傳送A群組文字頻道消息到B群組
-        if msg.channel.id == jdata['listen_channelID'] or msg.channel.id == jdata['None_channel'] and msg.author != self.bot.user:
+        if msg.channel.id == jdata['listen_channelID'] or msg.channel.id == jdata['Trans_test_channelID'] and msg.author != self.bot.user:
             #Debug
             #print(f'Fetched message: {msg}')
             #discord.Client.sniped_message = msg
