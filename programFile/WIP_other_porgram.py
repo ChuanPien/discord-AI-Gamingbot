c18e2a8abd72c63816c4c55aa3d69ae6f3ed5ba0
disbotMust <104551519+disbotMust@users.noreply.github.com>
2022-09-20 11:17:45 +0800

    新增格式檢查與錯誤輸出

--- programFile/WIP_other_porgram.py
+++ programFile/WIP_other_porgram.py
@@ -14,7 +14,7 @@ class test:
         # 使用者輸入空字串計數器
         input_list_none_count = 0
         # 初始化最終url
-        last_url = "錯誤的輸入格式"
+        last_url = None
         # 初始化流派(poe.ninja)url
         url_ninja = 'https://poe.ninja/challenge/builds?'
         # 建立元素列表
@@ -28,23 +28,26 @@ class test:
         print(f'input = {user_input},{len(user_input)}')
 
         # 迴圈檢查input並輸出對應結果
-        for i in range(0,len(url_element)):
-            stringContent = user_input[i]
-            # 如果空值則讓計數器增加
-            if stringContent == '':
-                input_list_none_count += 1
-            # 不是空值且是第一個條件
-            elif stringContent != '' and first_element == 0:
-                last_url = url_ninja+url_element[i]+stringContent
-                # 第一條件計數器+1
-                first_element = first_element + 1
-            # 非第一條件時合併其他條件
-            elif stringContent != '' and first_element != 0:
-                last_url = last_url+element_link+url_element[i]+stringContent
-            else:
-                last_url = "輸出錯誤"
-                print(input_list_none_count)
-                break
+        if len(user_input) == len(url_element):
+            for i in range(0,len(url_element)):
+                stringContent = user_input[i]
+                # 如果空值則讓計數器增加
+                if stringContent == '':
+                    input_list_none_count += 1
+                # 不是空值且是第一個條件
+                elif stringContent != '' and first_element == 0:
+                    last_url = url_ninja+url_element[i]+stringContent
+                    # 第一條件計數器+1
+                    first_element = first_element + 1
+                # 非第一條件時合併其他條件
+                elif stringContent != '' and first_element != 0:
+                    last_url = last_url+element_link+url_element[i]+stringContent
+                else:
+                    last_url = "輸出錯誤"
+                    print(input_list_none_count)
+                    break
+        else:
+            last_url = "錯誤的格式或其他未知錯誤"
         if input_list_none_count == len(user_input):
             last_url = "請至少輸入一項查詢項目"
             
