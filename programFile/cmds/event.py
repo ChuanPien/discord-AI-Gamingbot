020e07ce08db76daf64a6a653ecdd3c303013656
disbotMust <104551519+disbotMust@users.noreply.github.com>
2022-10-14 12:02:00 +0800

    Update event.py

--- programFile/cmds/event.py
+++ programFile/cmds/event.py
@@ -14,29 +14,8 @@ class Event(Cog_Extension):
     @commands.Cog.listener()
     # on_massage功能必須寫在同一處 不然會覆蓋
     async def on_message(self, msg):
-        # 設定傳送消息頻道
-        sendContentChannel = self.bot.get_channel(jdata['listen_test_channelID'])
-        # 初始化，設定專用log頻道
-        logChat = self.bot.get_channel(jdata['Log_chat'])
-        # msg抓取初始化
         if msg.author != self.bot.user:
-            msgFormat = msg.content
-            msgFormat = msgFormat.split(" ")
-            print(msgFormat)
-
-        # 測試log運作
-        if msg.author != self.bot.user:
-            for mf in msgFormat:
-                if mf == 'sec':
-                    # debug
-                    await msg.channel.send("log運作測試")
-                    # 初始化messageFormat
-                    msg_f = messageFormat(msg)
-                    msg_f.log()
-                    await logChat.send("－－－－－\n" + "ID: " + msg_f.getUserId() + "\nMsg: " + msg_f.getMsg() + "\n－－－－－")
-                else:
-                    print("\n\nError")
-        
+            pass
 
 
 def setup(bot):
